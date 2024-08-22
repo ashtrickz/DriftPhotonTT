3bb601d4ff6b1636aad24519bb7f19f350b253e8
AshTrickz <ashshvern@gmail.com>
2024-08-22 14:41:13 +0300

    Initnial commit

---
+++ Assets/DriftTTCore/Scripts/AI/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle {
+    public int node;
+    public string name;
+    public bool hasFinished;
+
+    public Vehicle(int node,string name,bool HasFinished){
+        this.node = node;
+        this.name = name;
+        hasFinished =HasFinished;
+    }
+}
---
+++ Assets/DriftTTCore/Scripts/Data/RootData.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/RootData")]
+public class RootData : SerializedScriptableObject
+{
+
+    #region Singleton
+
+    private static RootData _instance;
+    
+    public static RootData RootInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+#if UNITY_EDITOR
+                Debug.Log($"Loading... RootData instance");
+#endif
+                _instance = Resources.Load<RootData>("Data/RootData");
+                if (_instance == null)
+                {
+                    Debug.LogError("RootData doesn't found");
+#if UNITY_EDITOR
+                    Debug.Log($"Creating... RootData instance");
+#endif
+                    _instance = CreateInstance<RootData>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public Dictionary<string, GameObject> Vehicles = new();
+
+}
